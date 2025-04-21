5c21735552da8163755061dd79c547b938cdc892
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-04-21 22:10:43 +0530

    Time: 2017 ms (57.81%) | Memory: 98.4 MB (61.28%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        unordered_set<int> st, s;
+
+        int n = nums.size();
+
+        if(n == 1 || n == 2) return n;
+
+        for(int i = 0;i < n;i++){
+            for(int j = i + 1;j < n;j++){
+                st.insert(nums[i]^nums[j]);
+            }
+        }
+
+        for(auto num:st){
+            for(auto a:nums){
+                s.insert(num ^ a);
+            }
+        }
+
+        return s.size();
+    }
+};
