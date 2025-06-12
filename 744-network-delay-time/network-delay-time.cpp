15c3ab08419351609a1699d63c414665a65f55eb
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-06-12 18:57:41 +0530

    Time: 85 ms (58.44%) | Memory: 47.8 MB (39.17%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,23 +1,23 @@
-const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
+// const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
 
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
+// #define LC_HACK
+// #ifdef LC_HACK
+// const auto __ = []() {
+//     struct ___ {
+//         static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
+//     };
+//     std::atexit(&___::_);
+//     return 0;
+// }();
+// #endif
 
 
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // ios_base::sync_with_stdio(0);
-        // cin.tie(0);
-        // cout.tie(0);
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
 
         vector<pair<int, int>> adj[n + 1];
         for(auto it : times){
