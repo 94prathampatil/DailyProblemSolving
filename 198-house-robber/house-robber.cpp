788e59012a142d183ceef50257e26853d70ae70b
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-06-19 10:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (5.81%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -28,13 +28,31 @@ public:
         dp[i] = max(inc, exc);
         return dp[i];
     }
+    int solveTab(vector<int> &nums, int n){
+        if(n == 0)  return 0;
+        if(n == 1)  return nums[0];
+
+        vector<int> dp(n, 0);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2;i < n;i++){
+            int inc = dp[i - 2] + nums[i];
+            int exc = dp[i - 1];
+
+            dp[i] = max(inc, exc);
+        }
+
+        return dp[n - 1];
+    }
     int rob(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
         int n = nums.size();
-        vector<int> dp(n + 1, -1);
-        return solve(nums, 0, n, dp);
+        // vector<int> dp(n + 1, -1);
+        return solveTab(nums, n);
     }
 };
