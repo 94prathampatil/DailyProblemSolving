5644d507d657cd16e879ae59d98da2d80dad12ae
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-11 09:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (33.97%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -11,58 +11,33 @@ const auto __ = []() {
 }();
 #endif
 
-#define pb(x) push_back(x)
 
 class Solution {
 public:
-    int solve(int n, vector<int>& days, vector<int>& cost, int index, vector<int>& dp){
-        // Base case
-        if(index >= n)  return 0;
-
-        if(dp[index] != -1) return dp[index];
-
-        int singleDayPass = cost[0] + solve(n, days, cost, index + 1, dp);
-
-        // For 7 days pass
-        int i;
-        for(i = index; i < n && days[i] < days[index] + 7;i++);
-        int sevenDayPass = cost[1] + solve(n, days, cost, i, dp);
-
-        // for 30 days pass
-        for(i = index; i < n && days[i] < days[index] + 30;i++);
-        int thirtyDaysPass = cost[2] + solve(n, days, cost, i, dp);
-
-
-        return dp[index] = min(singleDayPass, min(sevenDayPass, thirtyDaysPass));
-    }
-
-    int solveTab(int n, vector<int>& days, vector<int>& cost){
-        vector<int> dp(n + 1, INT_MAX);
-        dp[n] = 0;
-
-        for(int i = n - 1; i >= 0;i--){
-            int singleDayPass = cost[0] + dp[i + 1];
-
-            int j;
-            for(j = i; j < n && days[j] < days[i] + 7;j++);
-            int sevenDayPass = cost[1] + dp[j];
-
-            for(j = i; j < n && days[j] < days[i] + 30;j++);
-            int thirtyDaysPass = cost[2] + dp[j];
-
-            dp[i] = min(singleDayPass, min(sevenDayPass, thirtyDaysPass));
-        }
-
-        return dp[0];
-    }
-
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
+    int mincostTickets(vector<int>& days, vector<int>& cost) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
-        int n = days.size();
-        // vector<int> dp(n + 1, -1);
-        return solveTab(n, days, costs);
+        int ans = 0;
+        queue<pair<int, int>> weekly, monthly;
+
+        for(auto day:days){
+            while(!monthly.empty() && monthly.front().first + 30 <= day){
+                monthly.pop();
+            }
+
+            while(!weekly.empty() && weekly.front().first + 7 <= day){
+                weekly.pop();
+            }
+
+            weekly.push({day, ans + cost[1]});
+            monthly.push({day, ans + cost[2]});
+
+
+            ans = min(ans + cost[0], min(weekly.front().second, monthly.front().second));
+        }
+
+        return ans;
     }
 };
