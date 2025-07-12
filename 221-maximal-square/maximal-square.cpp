715268fc3c29d2893005eb1b1f7cffb8937a75c0
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-12 18:01:42 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (90.41%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -36,26 +36,74 @@ public:
         }
     }
 
-    // Memoization Gives TLE... :( 
-    // int solveTab(int i, int j, vector<vector<char>> &matrix){
-    //     int n =  matrix.size(), m = matrix[0].size();
+    int solveTab(vector<vector<char>> &matrix, int &maxi){
+        int n =  matrix.size(), m = matrix[0].size();
 
-    //     vector<vector<int>> dp(n, vector<int> (m, 0));
-    //     dp[n][m] = 0;
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
 
-    //     for(int )
-    // }
+        for(int i = n - 1;i >= 0;i--){
+            for(int j = m - 1;j >= 0;j--){
+
+                int right = dp[i][j + 1];
+                int diagonal = dp[i + 1][j + 1];
+                int down = dp[i + 1][j];
+
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(dp[i][j], maxi);
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    int solveSO(vector<vector<char>> &matrix, int &maxi){
+        int n =  matrix.size(), m = matrix[0].size();
+
+        vector<int> curr(m + 1, 0);
+        vector<int> next(m + 1, 0);
+
+        for(int i = n - 1;i >= 0;i--){
+            for(int j = m - 1;j >= 0;j--){
+
+                int right = curr[j + 1];
+                int diagonal = next[j + 1];
+                int down = next[j];
+
+                if(matrix[i][j] == '1'){
+                    curr[j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(curr[j], maxi);
+                }
+                else{
+                    curr[j] = 0;
+                }
+            }
+
+            next = curr;
+        }
+
+        return next[0];
+    }
 
     int maximalSquare(vector<vector<char>>& matrix) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+        // vector<vector<int>> dp(n, vector<int> (m, -1));
+        // int maxi = 0;
+        // solve(0, 0, matrix, maxi, dp);
+        // return maxi * maxi;
+
         int maxi = 0;
-        solve(0, 0, matrix, maxi, dp);
+        solveSO(matrix, maxi);
         return maxi * maxi;
     }
 };
