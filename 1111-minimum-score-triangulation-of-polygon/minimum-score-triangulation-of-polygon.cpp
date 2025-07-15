4dc9278d7b0504de3d94cfe105363dd26ba7a3c8
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-15 10:00:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (14.45%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -29,13 +29,31 @@ public:
 
         return dp[i][j];
     }
+
+    int solveTab(vector<int>& v){
+        int n = v.size();
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+
+        for(int i = n - 1;i >= 0;i--){
+            for(int j = i + 2;j < n;j++){
+                int ans = INT_MAX;
+                for(int k = i + 1;k < j;k++){
+                    ans = min(ans, (v[i] * v[j] * v[k]) + dp[i][k] + dp[k][j]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+
     int minScoreTriangulation(vector<int>& v) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
-        int n = v.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return solve(v, 0, n - 1, dp);
+        // int n = v.size();
+        // vector<vector<int>> dp(n, vector<int> (n, -1));
+        return solveTab(v);
     }
 };
