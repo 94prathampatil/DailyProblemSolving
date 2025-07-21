d73e2271771d07c5920e9a0f291abe3ddb9d866f
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-21 15:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (45.44%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -31,13 +31,56 @@ public:
 
         return dp[curr][prev + 1] = max(take, notTake);
     }
+
+    int solveTab(vector<int> &nums){
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for(int curr = n - 1;curr >= 0;curr--){
+            for(int prev = curr - 1;prev >= -1;prev--){
+                int take = 0;
+                if(prev == -1 || nums[curr] > nums[prev]){
+                    take = 1 + dp[curr + 1][curr + 1];
+                }
+                int notTake = dp[curr + 1][prev + 1];
+
+                dp[curr][prev + 1] = max(take, notTake);
+            }
+        }
+
+        return dp[0][0];
+
+    }
+
+    int solveSO(vector<int> &nums){
+        int n = nums.size();
+        vector<int> currRow(n + 1, 0);
+        vector<int> nextRow(n + 1, 0);
+
+        for(int curr = n - 1;curr >= 0;curr--){
+            for(int prev = curr - 1;prev >= -1;prev--){
+                int take = 0;
+                if(prev == -1 || nums[curr] > nums[prev]){
+                    take = 1 + nextRow[curr + 1];
+                }
+                int notTake = nextRow[prev + 1];
+
+                currRow[prev + 1] = max(take, notTake);
+            }
+
+            nextRow = currRow;
+        }
+
+        return nextRow[0];
+    }
     int lengthOfLIS(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (n + 1, -1));
-        return solve(n, nums, 0, -1, dp);
+        // vector<vector<int>> dp(n, vector<int> (n + 1, -1));
+        // return solve(n, nums, 0, -1, dp);
+        return solveSO(nums);
     }
 };
