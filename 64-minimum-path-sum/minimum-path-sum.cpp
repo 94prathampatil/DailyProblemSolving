c2b45e41946a797547f7a3ea870bbecaedf2e992
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-27 12:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (14.63%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -32,15 +32,42 @@ public:
         return dp[x][y] = grid[x][y] + min(right, down);
     }
 
+    int solveTab(vector<vector<int>> &grid){
+        int n = grid[0].size();
+        int m = grid.size();
+
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        dp[m - 1][n - 1] = grid[m - 1][n - 1];
+
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m-1][j] = grid[m-1][j] + dp[m-1][j+1];
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
+        }
+
+        for(int x = m - 2;x >= 0;x--){
+            for(int y = n - 2;y >= 0;y--){
+                int right = dp[x + 1][y];
+                int down = dp[x][y + 1];
+
+                dp[x][y] = grid[x][y] + min(right, down);
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
         
-        int n = grid[0].size();
-        int m = grid.size();
+        // int n = grid[0].size();
+        // int m = grid.size();
 
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
-        return solve(grid, 0, 0, dp);
+        // vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
+        return solveTab(grid);
     }
 };
