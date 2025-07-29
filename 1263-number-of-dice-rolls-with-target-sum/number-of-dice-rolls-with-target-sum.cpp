1195e2856e0d134ca6e91f86f8dbec279c4df0f5
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-29 11:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (35.78%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -15,8 +15,10 @@ class Solution {
 public:
     int solve(int dice, int faces, int t, vector<vector<int>>& dp){
         // base Case
-        if (t < 0 || dice < 0 || (t == 0) != (dice == 0)) return 0;
-        if (t == 0 && dice == 0) return 1;
+        if(t < 0)   return 0;
+        if(dice != 0 && t == 0) return 0;
+        if(dice == 0 && t != 0) return 0;
+        if(dice == 0 && t == 0) return 1;
 
         if(dp[dice][t] != -1){
             return dp[dice][t];
@@ -29,13 +31,33 @@ public:
 
         return dp[dice][t] = (ans % m);
     }
+
+    int solveTab(int d, int f, int t){
+    vector<vector<int>> dp(d + 1, vector<int>(t + 1, 0));
+    dp[0][0] = 1;
+
+    for(int dice = 1; dice <= d; dice++){
+        for(int target = 1; target <= t; target++){
+            int ans = 0;
+            for(int i = 1; i <= f; i++){
+                if(target - i >= 0) {
+                    ans = (((ans % m) + (dp[dice - 1][target - i] % m)) % m);
+                }
+            }
+            dp[dice][target] = (ans % m);  
+        }
+    }
+
+    return (dp[d][t] % m);  
+}
+
     int numRollsToTarget(int n, int k, int target) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+        // vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
 
-        return (solve(n, k, target, dp) % m);
+        return (solveTab(n, k, target) % m);
     }
 };
