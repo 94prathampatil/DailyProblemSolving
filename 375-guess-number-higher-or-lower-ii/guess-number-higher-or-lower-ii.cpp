26ab7a9977d4d8efef604be6a095d3529661765e
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-31 10:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (6.70%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -26,11 +26,31 @@ public:
 
         return dp[start][end] = maxi;
     }
+
+    int solveTab(int n){
+        vector<vector<int>> dp(n + 2, vector<int> (n + 2, 0));
+
+        for(int start = n;start >= 1;start--){
+            for(int end = start;end <= n;end++){
+                if(start == end)    continue;
+                else{
+                    int maxi = INT_MAX;
+                    for(int i = start;i <= end;i++){
+                        maxi = min(maxi, i + max(dp[start][i - 1], dp[i + 1][end]));
+                    }
+
+                    dp[start][end] = maxi;
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
     int getMoneyAmount(int n) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
-        return solve(1, n, dp);
+        // vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
+        return solveTab(n);
     }
 };
