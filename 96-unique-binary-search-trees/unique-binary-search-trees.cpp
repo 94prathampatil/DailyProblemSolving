9193dd0d4c0acd973d262b674eb9190fe645437a
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-07-31 06:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (16.02%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -26,12 +26,26 @@ public:
 
         return dp[n] = ans;
     }
+
+    int solveTab(int n){
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = 1, dp[1] = 1;
+
+        for(int i = 2;i <= n;i++){
+            for(int j = 1;j <= i;j++){
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
     int numTrees(int n) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
         vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        return solveTab(n);
     }
 };
