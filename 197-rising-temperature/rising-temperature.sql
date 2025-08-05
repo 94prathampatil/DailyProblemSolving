6212d1172869560dd508ab933daa60c4bcb3db12
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-08-05 19:45:43 +0530

    Time: 703 ms (9.06%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,22 @@
+-- Step 1: Self Join with all values
+-- SELECT * 
+-- FROM 
+-- Weather w1 JOIN Weather w2;
 
-SELECT w1.id 
-FROM Weather w1 JOIN Weather w2 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+-- Step 2: Calculate the Date difference for previous day
+-- SELECT * FROM 
+-- Weather w1 JOIN Weather w2 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1;
+
+-- Step 3: Now we have a data of Previous and Current date so, we have to just calculate now the previous day temp > curr day temp then return it 
+
+-- SELECT * FROM 
+-- Weather w1 JOIN Weather w2 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+
+-- Step 4: Now we have all data, and we need only id, so print ID only 
+
+SELECT w1.id FROM 
+Weather w1 JOIN Weather w2 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+
+
+-- SELECT w1.id 
+-- FROM Weather w1 JOIN Weather w2 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
