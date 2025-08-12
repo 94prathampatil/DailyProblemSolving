4f5cc90fd811c0b73560688c3eda868406243488
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-08-12 16:18:43 +0530

    Time: 0 ms (100.00%) | Memory: 94.6 MB (62.62%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -35,10 +35,35 @@ public:
 
         return dp[i][isBuy] = profit;
     }
+
+    int solveTab(vector<int>& prices, int fee){
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for(int i = n - 1;i >= 0;i--){
+            for(int isBuy = 0; isBuy <= 1;isBuy++){
+                int profit = 0;
+                if(isBuy){
+                    int buy = -prices[i] + dp[i + 1][0];    
+                    int notBuy = dp[i + 1][1];
+                    profit = max(buy, notBuy);
+                }
+                else{
+                    int sell = prices[i] + dp[i + 1][1];
+                    int notSell = dp[i + 1][0];
+                    profit = max(sell - fee, notSell);
+                }
+
+                dp[i][isBuy] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices, int fee) {
         ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-        int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
-        return solve(0, 1, fee, prices, dp);
+        // int n = prices.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        return solveTab(prices, fee);
     }
 };
