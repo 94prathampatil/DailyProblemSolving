cd3a8f3929aa3322e2ef91dd36b1c8d47925aa55
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-08-12 19:45:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (88.88%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -31,15 +31,42 @@ public:
 
         return dp[i][j] = ans;
     }
+
+    int solveTab(string &t1, string &t2){
+        int n = t1.length();
+        int m = t2.length();
+
+        // vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        vector<int> curr(m + 1, 0);
+        vector<int> next(m + 1, 0);
+
+        for(int i = n - 1;i >= 0;i--){
+            for(int j = m - 1;j >= 0;j--){
+                int ans = 0;
+                if(t1[i] == t2[j]){
+                    ans = 1 + next[j + 1];
+                }
+                else{
+                    ans = max(curr[j + 1], next[j]);
+                }
+
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+
+        return curr[0];
+    }
     int longestCommonSubsequence(string t1, string t2) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
-        int n = t1.length();
-        int m = t2.length();
+        // int n = t1.length();
+        // int m = t2.length();
 
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
-        return solve(t1, t2, 0, 0, dp);
+        // vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        // return solve(t1, t2, 0, 0, dp);
+        return solveTab(t1, t2);
     }
 };
