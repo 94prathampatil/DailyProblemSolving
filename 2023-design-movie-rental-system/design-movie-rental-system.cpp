f1c8e1b9586f5cc7517df1fdb54203b32fbd69f0
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2025-09-21 14:47:49 +0530

    Time: 471 ms (13.14%) | Memory: 441.6 MB (26.28%) - LeetSync

---
+++ 2023-design-movie-rental-system/design-movie-rental-system.cpp
@@ -0,0 +1,71 @@
+class MovieRentingSystem {
+public:
+    set<vector<int>> rented;
+    unordered_map<int, set<vector<int>>> unRented;
+    unordered_map<int, unordered_map<int, int>> price;
+
+    MovieRentingSystem(int n, vector<vector<int>>& entries) {
+        for(auto &entry : entries){
+            int sh = entry[0];
+            int m = entry[1];
+            int p = entry[2];
+
+            unRented[m].insert({p, sh});
+            price[m][sh] = p;            
+        }
+    }
+    
+    vector<int> search(int movie) {
+        if(unRented.find(movie) == unRented.end()){
+            return {};
+        }
+
+        vector<int> ans;
+
+        for(auto i : unRented[movie]){
+            int shop = i[1];
+            ans.push_back(shop);
+
+            if(ans.size() == 5) break;
+        }
+
+        return ans;
+    }
+    
+    void rent(int shop, int movie) {
+        int p = price[movie][shop];
+        unRented[movie].erase(unRented[movie].find({p, shop}));
+
+        rented.insert({p, shop, movie});        
+    }
+    
+    void drop(int shop, int movie) {
+        int p = price[movie][shop];
+        rented.erase({p, shop, movie});
+        unRented[movie].insert({p, shop});
+    } 
+    
+    vector<vector<int>> report() {
+        vector<vector<int>> ans;
+
+        for(auto i: rented){
+            int shop = i[1];
+            int movie = i[2]; 
+            ans.push_back({shop, movie});
+
+            if(ans.size() == 5) break;
+        }
+
+
+        return ans;
+    }
+};
+
+/**
+ * Your MovieRentingSystem object will be instantiated and called as such:
+ * MovieRentingSystem* obj = new MovieRentingSystem(n, entries);
+ * vector<int> param_1 = obj->search(movie);
+ * obj->rent(shop,movie);
+ * obj->drop(shop,movie);
+ * vector<vector<int>> param_4 = obj->report();
+ */
