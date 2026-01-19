81d87acea99938b6d72d22a793adfe8ef6a64de9
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2026-01-19 11:08:38 +0530

    Time: 218 ms (5.00%) | Memory: 32.9 MB (12.27%) - LeetSync

---
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> pref(n, vector<int>(m, 0));
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int left = (j > 0) ? pref[i][j - 1] : 0;
+                int top = (i > 0) ? pref[i - 1][j] : 0;
+                int topLeft = (i > 0 && j > 0) ? pref[i - 1][j - 1] : 0;
+
+                pref[i][j] = left + top - topLeft + mat[i][j];
+            }
+        }
+
+        int ans = 0;
+
+        // Try all square sizes
+        for (int k = 1; k <= min(n, m); k++) {
+            for (int i = 0; i + k - 1 < n; i++) {
+                for (int j = 0; j + k - 1 < m; j++) {
+
+                    int i2 = i + k - 1;
+                    int j2 = j + k - 1;
+
+                    int sum = pref[i2][j2];
+                    if (i > 0) sum -= pref[i - 1][j2];
+                    if (j > 0) sum -= pref[i2][j - 1];
+                    if (i > 0 && j > 0) sum += pref[i - 1][j - 1];
+
+                    if (sum <= threshold) {
+                        ans = max(ans, k);
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
