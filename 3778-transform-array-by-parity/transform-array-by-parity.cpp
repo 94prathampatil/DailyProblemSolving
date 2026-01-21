9fdef12a9da1be1e8abbde8cda2ad1c851544ebb
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2026-01-21 11:22:29 +0530

    Time: 1 ms (26.64%) | Memory: 28.4 MB (25.38%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        for(int i = 0;i < nums.size();i++){
+            if(nums[i] & 1){
+                nums[i] = 1;
+            }
+            else{
+                nums[i] = 0;
+            }
+        }
+
+        sort(nums.begin(), nums.end());
+
+        return nums;
+    }
+};
