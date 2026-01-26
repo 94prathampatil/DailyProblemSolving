f645a5ef65d5e4745b541af987688d326c9ee8f0
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2026-01-26 13:00:16 +0530

    Time: 7 ms (16.03%) | Memory: 29.5 MB (9.57%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -1,37 +1,85 @@
+typedef long long ll;
 #define all(x) x.begin(), x.end()
 
 class Solution {
 public:
-    int minSum(vector<int> &nums){        
-        int mSum = INT_MAX;
-        int index = -1;
-
-        for(int i = 0;i < nums.size() - 1;i++){
-            if(nums[i] + nums[i + 1] < mSum){
-                index = i;
-                mSum = nums[i] + nums[i + 1];
-            }
-        }
-
-        return index;
-    }
     int minimumPairRemoval(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
+        
+        int n = nums.size();
 
-        int opr = 0;
+        vector<ll> temp(all(nums));
 
-        while(!is_sorted(all(nums))){
-            int i = minSum(nums);
+        set<pair<ll, int>> minPairSet;
 
-            nums[i] = nums[i] + nums[i + 1];
+        vector<int> nextIndex(n), prevIndex(n);
 
-            nums.erase(nums.begin() + i + 1);
-
-            opr++;
+        for(int i = 0; i < n; i++){
+            nextIndex[i] = i + 1;
+            prevIndex[i] = i - 1;
         }
 
-        return opr;
+        int badPairs = 0;
+        for(int i = 0; i < n - 1; i++){
+            if(temp[i] > temp[i + 1])
+                badPairs++;
+
+            minPairSet.insert({temp[i] + temp[i + 1], i});
+        }
+
+        int oper = 0;
+
+        while(badPairs){
+            auto it = minPairSet.begin();
+            int first = it->second;
+            int second = nextIndex[first];
+
+            minPairSet.erase(it);
+
+            if(second >= n) continue;  // safety check
+
+            int first_left = prevIndex[first];
+            int second_right = nextIndex[second];
+
+            // Remove old bad pair (first, second)
+            if(temp[first] > temp[second])
+                badPairs--;
+
+            // Check (first_left, first)
+            if(first_left >= 0){
+                bool oldBad = temp[first_left] > temp[first];
+                bool newBad = temp[first_left] > temp[first] + temp[second];
+
+                if(oldBad && !newBad) badPairs--;
+                if(!oldBad && newBad) badPairs++;
+
+                minPairSet.erase({temp[first_left] + temp[first], first_left});
+                minPairSet.insert({temp[first_left] + temp[first] + temp[second], first_left});
+            }
+
+            // Check (second, second_right)
+            if(second_right < n){
+                bool oldBad = temp[second] > temp[second_right];
+                bool newBad = temp[first] + temp[second] > temp[second_right];
+
+                if(oldBad && !newBad) badPairs--;
+                if(!oldBad && newBad) badPairs++;
+
+                minPairSet.erase({temp[second] + temp[second_right], second});
+                minPairSet.insert({temp[first] + temp[second] + temp[second_right], first});
+
+                prevIndex[second_right] = first;
+            }
+
+            nextIndex[first] = second_right;
+
+            temp[first] += temp[second];
+
+            oper++;
+        }
+
+        return oper;
     }
 };
