4e8cf31bff67a1d631a3c8bf98b0059695cbb558
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2026-02-02 11:36:37 +0530

    Time: 491 ms (21.88%) | Memory: 185 MB (14.06%) - LeetSync

---
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    typedef long long ll;
+    typedef pair<ll, ll> P;
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        int n = nums.size();
+
+        set<P> kMin;
+        set<P> remain;
+
+        ll sum = 0;
+
+        int i = 1;
+        while(i - dist < 1){
+            kMin.insert({nums[i], i});
+            sum += nums[i];
+
+            if(kMin.size() > k - 1){
+                P temp = *kMin.rbegin();
+                sum -= temp.first;
+                remain.insert(temp);
+                kMin.erase(temp);
+            }
+
+            i++;
+        }
+
+        ll res = LONG_MAX;
+        while(i < n){
+            kMin.insert({nums[i], i});
+            sum += nums[i];
+
+            if(kMin.size() > k - 1){
+                P temp = *kMin.rbegin();
+                sum -= temp.first;
+                remain.insert(temp);
+                kMin.erase(temp);
+            }
+
+            res = min(sum, res);
+
+            // window shifting
+            P remove = {nums[i - dist], i - dist};
+            if(kMin.count(remove)){
+                kMin.erase(remove);
+                sum -= remove.first;
+
+                if(!remain.empty()){
+                    P temp = *remain.begin();
+                    kMin.insert(temp);
+
+                    sum += temp.first;
+
+                    remain.erase(temp);
+                }
+            }    
+            else{
+                remain.erase(remove);
+            }
+
+            i++;
+        }
+
+        return nums[0] + res;
+    }
+};
