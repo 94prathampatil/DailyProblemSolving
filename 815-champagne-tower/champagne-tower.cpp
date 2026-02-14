7872135df8124cacca879220af724439f69df240
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2026-02-14 13:40:37 +0530

    Time: 8 ms (33.66%) | Memory: 45.9 MB (9.44%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -19,13 +19,42 @@ public:
 
         return dp[i][j] = left + right; 
     }
+
+    double solveTab(int &poured, int &query_row, int &query_glass){
+        vector<vector<double>> dp(101, vector<double>(101, 0));
+
+        dp[0][0] = poured;
+
+        for(int i = 1; i <= query_row; i++){
+            for(int j = 0; j <= i; j++){
+
+                double val = 0;
+
+                if(j - 1 >= 0){
+                    double left = (dp[i - 1][j - 1] - 1) / 2.0;
+                    if(left > 0) val += left;
+                }
+
+                if(j <= i - 1){
+                    double right = (dp[i - 1][j] - 1) / 2.0;
+                    if(right > 0) val += right;
+                }
+
+                dp[i][j] = val;
+            }
+        }
+
+        return dp[query_row][query_glass];
+    }
+
     double champagneTower(int poured, int query_row, int query_glass) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
-        vector<vector<double>> dp(101 + 1, vector<double> (101 + 1, -1));
+        // vector<vector<double>> dp(101 + 1, vector<double> (101 + 1, -1));
 
-        return min(1.0, solve(query_row, query_glass, poured, dp));
+        double ans = solveTab(poured, query_row, query_glass);
+        return min(1.0, ans);
     }
 };
