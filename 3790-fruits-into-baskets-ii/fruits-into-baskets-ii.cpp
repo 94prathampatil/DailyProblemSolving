b686f3db0f58c6690e32649520a933fd63bfc2e5
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2026-02-26 12:02:25 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (5.23%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -1,26 +1,56 @@
 class Solution {
 public:
+    void buildTree(int i, int l, int r, vector<int> &segment, vector<int>& baskets){
+        if(l == r){
+            segment[i] = baskets[l];
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        buildTree(2 * i + 1, l, mid, segment, baskets);
+        buildTree(2 * i + 2, mid + 1, r, segment, baskets);
+
+        segment[i] = max(segment[2 * i + 1], segment[2 * i + 2]);
+
+        return;
+    }
+
+    bool query(int i, int l, int r, vector<int> &segment, int fruit){
+        if(segment[i] < fruit)  return false;
+
+        if(l == r){
+            segment[i] = -1;
+            return true;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        bool placed = false;
+
+        if(segment[2 * i + 1] >= fruit){
+            placed = query(2 * i + 1, l, mid, segment, fruit);
+        }
+        else{
+            placed = query(2 * i + 2, mid + 1, r, segment, fruit);
+        }
+
+        segment[i] = max(segment[2 * i + 1], segment[2 * i + 2]);
+
+        return placed;
+    }
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
+        int n = fruits.size();
+        
+        int unplaced = 0;
+        vector<int> segment(4 * n, -1);
+        buildTree(0, 0, n - 1, segment, baskets);
 
-        int ans = 0;
-        for(int i = 0;i < fruits.size();i++){
-            for(int j = 0;j < baskets.size();j++){
-                if(baskets[j] >= fruits[i]){
-                    baskets[j] = -1;
-                    break;
-                }
-            }
+        for(auto &fruit : fruits){
+            if(query(0, 0, n - 1, segment, fruit) == false)
+                unplaced++;
         }
 
-        for(auto &i:baskets){
-            if(i != -1){
-                ans++; 
-            }
-        }
-
-        return ans;
+        return unplaced;
     }
 };
