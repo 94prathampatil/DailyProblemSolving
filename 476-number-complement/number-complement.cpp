19dbc5750509900151e318e4a8ec54c068ab978d
Prathamesh Patil <127184152+94prathampatil@users.noreply.github.com>
2026-03-11 22:57:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (7.36%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,16 +1,16 @@
+#include<bits/stdc++.h>
+
 class Solution {
 public:
     int findComplement(int num) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-
-        int mask = 1;
-
-        while(mask < num){
-            mask = (mask << 1) | 1;
-        }
+        
+        int bit = floor(log2(num) + 1);
+        int mask = pow(2, bit) - 1;
 
         return num ^ mask;
+
     }
 };
